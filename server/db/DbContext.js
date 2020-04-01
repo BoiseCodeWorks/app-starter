3665ed860cf6e5ee87e1c5eefce97ec32f1a45a8
Jake Overall <joverall22@gmail.com>
2020-04-01 16:44:31 -0600

    initial commit

---
+++ server/db/DbContext.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import ValueSchema from "../models/Value";
+import ProfileSchema from "../models/Profile";
+import { PinSchema } from "../models/Pin";
+import FavoriteSchema from "../models/Favorite";
+
+class DbContext {
+  Values = mongoose.model("Value", ValueSchema);
+
+  Pins = mongoose.model("Pin", PinSchema);
+
+  Profile = mongoose.model("Profile", ProfileSchema);
+
+  Favorites = mongoose.model("Favorite", FavoriteSchema);
+
+}
+
+export const dbContext = new DbContext();
