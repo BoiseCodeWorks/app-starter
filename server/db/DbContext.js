983d4b9d58d971ee1bfea2152eae275eebea5274
Jake Overall <joverall22@gmail.com>
2020-04-01 17:57:44 -0600

    upate package

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,17 +1,11 @@
 import mongoose from "mongoose";
 import ValueSchema from "../models/Value";
 import ProfileSchema from "../models/Profile";
-import { PinSchema } from "../models/Pin";
-import FavoriteSchema from "../models/Favorite";
+
 
 class DbContext {
-  Values = mongoose.model("Value", ValueSchema);
-
-  Pins = mongoose.model("Pin", PinSchema);
-
   Profile = mongoose.model("Profile", ProfileSchema);
-
-  Favorites = mongoose.model("Favorite", FavoriteSchema);
+  Values = mongoose.model("Value", ValueSchema);
 
 }
 
